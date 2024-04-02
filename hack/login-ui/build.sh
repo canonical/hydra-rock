cd8df77a90a0c80b8780d8d0e37bbc54aec058e1
Nikos <nikos.sklikas@canonical.com>
2024-04-02 18:03:08 +0300

    temp: build hydra from branch

--- hack/login-ui/build.sh
+++ hack/login-ui/build.sh
@@ -5,8 +5,8 @@ set -x
 # it also requires sudo permissions to run skopeo
 
 # export version=$(yq -r '.version' rockcraft.yaml)
-rockcraft pack -v
+# rockcraft pack -v
 
-sudo skopeo --insecure-policy copy "oci-archive:identity-platform-login-ui_$(yq -r '.version' rockcraft.yaml)_amd64.rock" docker-daemon:$IMAGE
+skopeo --insecure-policy copy "oci-archive:identity-platform-login-ui_$(yq -r '.version' rockcraft.yaml)_amd64.rock" docker-daemon:$IMAGE
 
 docker push $IMAGE
