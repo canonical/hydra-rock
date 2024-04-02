43943f31b41f9e02810e8413382b4f45f3f8393a
Nikos <nikos.sklikas@canonical.com>
2024-04-02 18:03:08 +0300

    fix: WIP build hydra from branch

--- hack/login-ui/build.sh
+++ hack/login-ui/build.sh
@@ -5,7 +5,7 @@ set -x
 # it also requires sudo permissions to run skopeo
 
 # export version=$(yq -r '.version' rockcraft.yaml)
-rockcraft pack -v
+# rockcraft pack -v
 
 sudo skopeo --insecure-policy copy "oci-archive:identity-platform-login-ui_$(yq -r '.version' rockcraft.yaml)_amd64.rock" docker-daemon:$IMAGE
 
